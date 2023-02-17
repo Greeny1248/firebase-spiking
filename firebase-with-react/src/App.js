f45270db09ba0e4377d2cb1395a83380e5e1faa5
Josh <greeny1248@gmail.com>
2023-02-17 09:19:02 +0000

    testing esxternal audio api

--- firebase-with-react/src/App.js
+++ firebase-with-react/src/App.js
@@ -26,7 +26,7 @@ function App() {
     handleSubmit(dataRef.current.value);
     dataRef.current.value = "";
   };
-
+  //////////////////////////////////External synth keyboard//////////////////////////////////////////////
   const [playSound1] = useSound(
     "https://firebasestorage.googleapis.com/v0/b/fir-with-react-c4cc3.appspot.com/o/140-bpm-amen-break-original-processed-6945.mp3?alt=media&token=399fc329-1afe-44c4-9fa6-7ff971544948"
   );
@@ -70,6 +70,52 @@ function App() {
       // document.removeEventListener('keydown', onKeyPress);
     };
   }, [onKeyPress]);
+  ///////////////////////////////////MODULATION//////////////////////////////////////////////
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
+
+  const context = new AudioContext();
+  const masterVolume = context.createGain();
+  masterVolume.connect(context.destination);
+
+  const startButton = document.querySelector("#start");
+  const stopButton = document.querySelector("#stop");
+  const volumeControl = document.querySelector("#volume-control");
+  masterVolume.gain.value = 0.1;
+
+  volumeControl.addEventListener("input", changeVolume);
+
+  function changeVolume() {
+    masterVolume.gain.value = this.value;
+  }
+
+  const waveforms = document.getElementsByName("waveform");
+  let waveform;
+
+  function setWaveform() {
+    for (var i = 0; i < waveforms.length; i++) {
+      if (waveforms[i].checked) {
+        waveform = waveforms[i].value;
+      }
+    }
+  }
+
+  startButton.addEventListener("click", function () {
+    const oscillator = context.createOscillator();
+    oscillator.frequency.setValueAtTime(220, 0);
+    oscillator.connect(masterVolume);
+    oscillator.start(0);
+    oscillator.type = waveform;
+    stopButton.addEventListener("click", function () {
+      oscillator.stop(0);
+      delete oscillator;
+    });
+    waveforms.forEach((waveformInput) => {
+      waveformInput.addEventListener("change", function () {
+        setWaveform();
+        oscillator.type = waveform;
+      });
+    });
+  });
 
   return (
     <div className="App">
